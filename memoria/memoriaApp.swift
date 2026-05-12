7ed821e67629033839e47c72015b15cd667632fd
ria yahshua <yahshua.abuemeri@gmail.com>
2026-05-12 23:01:36 +0800

    Initial Commit

---
+++ memoria/memoriaApp.swift
@@ -0,0 +1,17 @@
+//
+//  memoriaApp.swift
+//  memoria
+//
+//  Created by YAHSHUA Systech on 5/12/26.
+//
+
+import SwiftUI
+
+@main
+struct memoriaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
